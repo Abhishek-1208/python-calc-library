89faf834ce2365f2c140950fbed80e200de227f1
Abhishek <shekabhi1208@gmail.com>
2024-01-09 17:52:19 +0000

    feat: added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="python_temp_library",
+    version="0.0.1",
+    author="Abhishek Saini",
+    author_email="shekabhi1208@gmail.com",
+    packages=find_packages(exclude=["calculator/test"]),
+    description="simple calculator",
+    url="https://github.com/Abhishek-1208/python-temp-library",
+    install_requires=[],
+    python_requires='>=3.9',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
